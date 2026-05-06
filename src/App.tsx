823db91e987ed89b72da6b26f19e405871b81bcb
jonathanwxh-cell <258340642+jonathanwxh-cell@users.noreply.github.com>
2026-05-06 15:29:08 +0800

    Add 1956 visual identity

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import './index.css';
 import Game from './components/Game';
+import { useEraAttribute } from './styles/useEraAttribute';
 
 function App() {
+  useEraAttribute();
+
   return <Game />;
 }
 
